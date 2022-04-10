1f986d36c43b0b1720413c93f9f553fe7d2b384c
Aier233 <93068627+Aier233@users.noreply.github.com>
2022-04-10 11:25:27 +0800

    新增登出 新增快速登陆

--- src/page/catalog/catalog-main/index.js
+++ src/page/catalog/catalog-main/index.js
@@ -1,2 +1,17 @@
 // require('page/common/header/index.js');
+var _util = require('util/util.js');
 
+const fastsignin = function(){
+
+    _util.request({
+        method  : 'POST',
+        url     : _util.getSeverURL('account/login'),
+        data    : {username : '123',password : '123'},
+        success : function(){
+            window.location.href = "../../view/catalog/catalog-main.html";
+        },
+    })
+
+}
+
+$('#fast_signin').on('click',fastsignin);
