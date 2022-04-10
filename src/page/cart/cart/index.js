ace4c08494c92591873ce5a241ea091dd2661b07
Aier233 <93068627+Aier233@users.noreply.github.com>
2022-04-10 18:39:17 +0800

    购物车前端

--- src/page/cart/cart/index.js
+++ src/page/cart/cart/index.js
@@ -1,8 +1,9 @@
 var _util = require('util/util.js');
 var _account_service = require('service/account-service.js');
+import {currency} from 'util/currency'
+Vue.filter("currency",currency);
 
-
-
+//判断用户是否存在
 const getUsername = function(){
 
     return new Promise(function(resolve,reject){
@@ -24,9 +25,8 @@ const getUsername = function(){
     });
     
 }
-
+//渲染购物车
 const viewCart = function(username){
-
     return new Promise(function(resolve,reject){
         _util.request({
             url : _util.getSeverURL('cart/getCart'),
@@ -35,8 +35,7 @@ const viewCart = function(username){
                 if(msg=="购物车为空"){
                     cartEmpty();
                 }else{
-                    formCart(data);
-                    console.log(data);
+                    formCart(data,username);
                 }
                 
             },
@@ -48,39 +47,103 @@ const viewCart = function(username){
     });
 }
 
-const formCart = function(data){
+//在购物车内数量不为零时Vue渲染购物车
+const formCart = function(data,username){
     var app = new Vue ({
         el  : '#carttable',
         data: {
             cartIsEmpty   : false,
             cartIsNotEmpty: true,
             cart   : data,
-            updateCartItem:function(){
+            
+            updateCartItem(item){
+                
+                var id = item.target.getAttribute('name');
+                var quantity = item.target.value;
+                xhr = new XMLHttpRequest();
+                xhr.onreadystatechange = update;
+                xhr.open("POST", "http://localhost:8090/cart/updateCartItem?itemId="+id+"&quantity="+quantity+"&username="+username, true);
+                xhr.send(null);
+                _util.request({
+                    url : _util.getSeverURL('cart/getCart'),
+                    data: { username : username },
+                });
+
 
             },
+            removeItemFromCart(event){
+                var id = event.target.getAttribute('name');
+                _util.request({
+                    url     : _util.getSeverURL('cart/removeItemFromCart'),
+                    data    : {itemId : id,username : username},
+                });
+                _util.request({
+                    url : _util.getSeverURL('cart/getCart'),
+                    data: { username : username },
+                });
+                var row = document.getElementById(id);
+                document.getElementById("carttable").deleteRow(row.rowIndex);
+            }
+            
+            
         }
     });
 }
-
+//在购物车为空时隐藏表单
 const cartEmpty = function(){
     var emptyCartMSG = new Vue ({
         el  : '#carttable',
         data: {
             cartIsEmpty     : true,
             cartIsNotEmpty  : false,
+            cart            : null,
         }
     });
 }
 
+//异步操作，首先判断用户是否存在，然后渲染购物车
 //不能使用同步操作，因为要访问后端导致延迟，所以需要采用异步方法
 async function showCart(){
     let username = await getUsername();
     await viewCart(username);
 }
 
+
+
+//手动输入数量更新购物车
+var xhr;
+function update() {
+    if(xhr.readyState === 4){
+        if(xhr.status === 200){
+            var result = xhr.responseText;
+            var newItem = eval("("+result+")");
+            var isRemoved = newItem.isRemoved;
+            var m = document.getElementById("m");
+
+            if(isRemoved == "false"){
+                var item = document.getElementsByName(newItem.itemId);
+                item.innerText = newItem.quantity;
+                document.getElementById("itemtotalcost"+newItem.itemId).innerHTML=newItem.html;
+            }
+            else if(isRemoved == "true"){
+                var row = document.getElementById(newItem.itemId);
+                document.getElementById("carttable").deleteRow(row.rowIndex);
+            }
+        }
+    }
+}
+
+//Remove删除按钮
+function remove(){
+
+}
+
 $(document).ready(function(){
 
+    //渲染页面
     showCart();
+
+    
 
 })
 
